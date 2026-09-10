f8790a34dffdcb1d32951f69c95630558236e87a
Anthony Rubin <support@beemoo.com>
2026-09-09 22:58:09 -0400

    Initial Commit

---
+++ BeemooArticulationTests/BeemooArticulationTests.swift
@@ -0,0 +1,18 @@
+//
+//  BeemooArticulationTests.swift
+//  BeemooArticulationTests
+//
+//  Created by Anthony Rubin on 9/9/26.
+//
+
+import Testing
+
+struct BeemooArticulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
